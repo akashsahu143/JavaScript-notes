726bee17435de241f106df23ca69a489ae605f96
Akash kumar saw <akashsahu105677@gmail.com>
2024-12-15 21:57:11 +0530

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,7 @@
+console.log("Hello ");
+
+let first_name = "Akash";
+let last_name = "saw";
+
+console.log(first_name +" " +last_name);
+
---
+++ operator of  js/Arithmetic.js
@@ -0,0 +1,48 @@
+//The Assignment Operator (=) assigns values
+ let a = 5;
+ let b = 5;
+
+//Arithmetic Operators 
+
+//Addition operators(+)
+
+let c = a+b;
+console.log(c);
+
+//  (-)	Subtraction
+
+let d = a-b;
+console.log(d);
+
+//(*) multiplication
+
+let e = a*b;
+console.log(e);
+
+// (/) divison
+
+let f = a/b;
+console.log(f);
+
+// (%)module
+
+let g = a%b;
+console.log(g);
+
+// Exponentiation(**)
+
+let h = a**b;
+console.log(h);
+
+//increment (++)
+
+let i = 6;
+
+let j = i++; //6+1 = 7
+console.log(i);
+
+//Decrement (--)
+
+let x =5;
+let y = x--; //5-1 = 4
+console.log(x);
