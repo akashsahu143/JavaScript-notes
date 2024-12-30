aa3ff435bafe3896d1801a0a0c57a7327b3e901b
Akash kumar saw <akashsahu105677@gmail.com>
2024-12-30 10:04:22 +0530

    Add files via upload

---
+++ loopandcontrol/infinity.js
@@ -0,0 +1,11 @@
+// //don't try becuse it will never stop and its effect to compiler 
+
+// let number = 0;
+
+// //condition
+// let i = 1 ;
+// while (i >0) {
+//     //infinte loop
+//     console.log(i);
+//     i = i + 1;
+//     }
