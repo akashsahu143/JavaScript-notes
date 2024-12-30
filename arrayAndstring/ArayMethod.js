21cc5ccb1931fb269b86441c4e34df986068f59b
Akash kumar saw <akashsahu105677@gmail.com>
2024-12-30 10:51:23 +0530

    Add files via upload

---
+++ arrayAndstring/ArayMethod.js
@@ -0,0 +1,27 @@
+//there are 5 method in this list
+
+//unshif()  : it removes the first element from the array.
+//shift()   : it removes the first element from the array.
+//pop()     : it removes the last element from the array.
+//splice()  : it removes the element from the array at the specified position.
+//push()    : it adds the element at the end of the array.
+
+
+//unshift() method
+
+let x = [1, 2, 3, 4, 5];
+
+console.log(x);
+
+console.log(x.unshift(0))
+console.log(x);
+
+//shift() method
+
+console.log(x.shift())
+console.log(x);
+
+//splice () method
+
+console.log(x.splice(3))
+console.log(x);
---
+++ arrayAndstring/array.js
@@ -0,0 +1,8 @@
+let x = [1,2,3];
+
+let y = x;
+
+x[0]= 1000;
+
+console.log(x);
+console.log(y);
